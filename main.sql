448edcc4709b88b4c2f19335b7e15444e36de0d5
João Colla <160884809+JoaoColla@users.noreply.github.com>
2024-08-14 11:12:28 -0300

    Add files via upload

---
+++ main.sql
@@ -0,0 +1,17 @@
+create table AGV (
+Nome TEXT NOT NULL,
+Turma TEXT NOT NULL,
+Idade INTEGER PRIMARY KEY,
+Bairro TEXT NOT NULL
+);
+
+INSERT INTO AGV ('Nickolas', '2 DS', 17 , 'Emboguaçu')
+INSERT INTO AGV ('Ventura', '2 DS', 'Emboguaçu')
+INSERT INTO AGV ('Matheus Araujo', '2 DS', 'Emboguaçu')
+INSERT INTO AGV ('Ana Flavia', '2 DS', 'Emboguaçu')
+INSERT INTO AGV ('João Possas', '2 DS', 'Emboguaçu')
+INSERT INTO AGV ('Rebeca', '2 DS', 'Emboguaçu')
+INSERT INTO AGV ('João Colla', '2 DS', 'El dorado')
+INSERT INTO AGV ('Izael' '2 DS', 'Jardim esperança')
+
+select * from AGV 
